2b46c35bd623d378e6648a85ab9c4ee8e2a59812
juliank <julian.kanjere@jskdezign.com>
2020-09-22 22:45:09 +0200

    Update rename and add ignore list

--- scripts/16_bulk_file_rename.py
+++ scripts/16_bulk_file_rename.py
@@ -22,6 +22,9 @@ import optparse
 FILEPATH_CURRENT_DIRECTORY = os.getcwd() # folder of current python script
 FILE_SUBMISSION_PREFIX = 'list_of_filenames_'
 FILE_EXT_XLSX = '.xlsx'
+EXCEL_SHEET_NAME = 'FilenameSheet'
+COLUMN_HEADERS = ['Old Name', 'New Name']
+FILES_TO_IGNORE = ['.DS_Store'] #ignore list
 
 def setup_logging(options):
     try:
@@ -44,51 +47,94 @@ def check_arguments(options):
 def read_files(options):
     logging.info('Reading filenames started - ' + options.path + ' ' + options.ext)
 
-
-    #  save all these variables in a single dataframe
-    column_headers = ['Old Name', 'New Name']
-
     # check filenames:
     if (options.ext==''):
         logging.info('Extension not set, will list all files')
-        old_filenames = [f for f in listdir(options.path) if isfile(join(options.path, f))]
-
+        old_filenames = [f for f in listdir(options.path) if isfile(join(options.path, f)) and f not in FILES_TO_IGNORE]
     else:
         logging.info('Extension is set, will list all .%s files' % options.ext)
-        old_filenames = [f for f in listdir(options.path) if isfile(join(options.path, f)) and  f.endswith(options.ext)]
+        old_filenames = [f for f in listdir(options.path) if isfile(join(options.path, f)) and f.endswith(options.ext)
+                         and f not in FILES_TO_IGNORE]
 
+    # sort the filenames in ascending order
+    old_filenames.sort()
 
-    filename_data = pd.DataFrame({'Old Name': old_filenames,
-                                  'New Name': None,
-                              })[column_headers]
+    filename_data = pd.DataFrame({COLUMN_HEADERS[0]: old_filenames,
+                                  COLUMN_HEADERS[1]: None,
+                              })[COLUMN_HEADERS]
 
     timestr = time.strftime('%Y%m%d-%H%M%S')
     filename = FILE_SUBMISSION_PREFIX + timestr + FILE_EXT_XLSX
     fullfilename = os.path.join(FILEPATH_CURRENT_DIRECTORY, filename)
     logging.info('Excel filename %s' %  fullfilename)
-    filename_data.to_excel(fullfilename)
+    filename_data.to_excel(fullfilename, sheet_name=EXCEL_SHEET_NAME)
     logging.info('Export to Excel complete')
     logging.info('Reading filenames finished')
 
 ### READ FILE LIST FROM EXCEL AND RENAME ON DISK ###
 def rename_files(options):
-    logging.info('Renaming filenames started - ' + options.path + ' ' + options.ext)
-    pass
-    #TODO - options.path, options.mapping_file
-    #f = ''
-    #g = ''
-    #if isfile(join(options.path, f)):
-    #    os.rename(join(options.path, f), join(options.path, g))
-    #print if there are problematic files
-    #commit excel file to research repos
-    logging.info('Renaming filenames finished')
+    logging.info('Renaming filenames initialising - ' + options.path + ' ' + options.mapping_file)
+    full_filename = join(options.path, options.mapping_file)
+    if isfile(full_filename):
+
+        excel_data_df = pd.read_excel(full_filename, sheet_name=EXCEL_SHEET_NAME)
+
+        # print whole sheet data
+        logging.debug('Excel data below:')
+        logging.debug(excel_data_df)
+
+        # list of column names in Excel
+        excel_data_df_columns = excel_data_df.columns.ravel()
+        logging.debug('Excel data columns below:')
+        logging.debug(excel_data_df_columns)
+
+        # original filenames
+        excel_data_df_orig_filenames = excel_data_df[COLUMN_HEADERS[0]].tolist()
+        logging.debug('List of old filenames %s' % excel_data_df_orig_filenames)
+
+        # new filenames
+        excel_data_df_new_filenames = excel_data_df[COLUMN_HEADERS[1]].tolist()
+        logging.debug('List of new filenames %s' % excel_data_df_new_filenames)
+
+       # sanity check - column lengths
+        len_excel_data_df_orig_filenames = len(excel_data_df_orig_filenames)
+        len_excel_data_df_new_filenames = len(excel_data_df_new_filenames)
+
+        logging.info('# of old filenames %s' % len_excel_data_df_orig_filenames)
+        logging.info('# of new filenames %s' % len_excel_data_df_new_filenames)
+
+        if len_excel_data_df_new_filenames != len_excel_data_df_orig_filenames:
+            logging.error('Number of new filenames (%s) does not match number of old filenames (%s). Exiting' %
+                          (len_excel_data_df_new_filenames, len_excel_data_df_orig_filenames))
+            sys.exit()
+
+        # sanity check - null values
+        if excel_data_df[COLUMN_HEADERS[1]].isnull().values.any():
+            logging.error('The new filenames contain some null values (%s entries). Exiting' %
+                          (excel_data_df[COLUMN_HEADERS[1]].isnull().sum()))
+            sys.exit()
+
+        logging.info('Renaming filenames starting - ' + options.path + ' ' + options.mapping_file)
+
+        for count, new_name_entry in enumerate(excel_data_df_new_filenames):
+            old_name_entry = excel_data_df_orig_filenames[count]
+            old_name_fullpath = join(options.path, old_name_entry)
+            if isfile(old_name_fullpath):
+                os.rename(old_name_fullpath, join(options.path, new_name_entry))
+            else:
+                logging.info('Skipping %s as it is not a valid file' % old_name_fullpath)
+        logging.info('Renaming filenames finished')
+    else:
+        logging.error('Invalid file or file does not exist - %s. Exiting' % full_filename)
+        sys.exit()
 
 ### DRIVER CODE ###
 if __name__ == '__main__':
-
     args = sys.argv[1:]
     parser = optparse.OptionParser('usage: python3 %prog --path <path> --ext <ext> --filename <mapping_file> --mode <read> --loglevel <loglevel> '
-                                   "e.g. python3 %prog --path '/Users/username/ResearchPaper/' --ext ''  --loglevel DEBUG")
+                                   "e.g. "
+                                   "read $python3 %prog --path '/Users/username/ResearchPaper/' --ext '' --mode 'read' --loglevel DEBUG"
+                                   "rename $python3 %prog --path '/Users/username/ResearchPapers1' --ext '' --filename '/Users/username/list_of_filenames_20200922-221324.xlsx' --mode 'rename' --loglevel DEBUG")
 
     parser.add_option('-p', '--path', dest='path', default='', help='The top level directory to check for files')
     parser.add_option('-e', '--ext', dest='ext', default='', help='The file extension to look for')
