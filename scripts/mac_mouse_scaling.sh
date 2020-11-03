8da45a7e7fb611cbb73071b9818195baa7319d0d
Julian Kanjere <2832754+jajukajulz@users.noreply.github.com>
2020-11-03 12:58:56 +0200

    Add 17_mac_mouse_scaling.sh for mouse speed

---
+++ scripts/mac_mouse_scaling.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#How to speed up mouse tracking on a Mac
+#$ chmod a+x 17_mac_mouse_scaling.sh
+#$ ./mac_mouse_scaling.sh
+
+#read current scaling speed (deafult is 3)
+defaults read -g com.apple.mouse.scaling
+
+#set speed to 7
+defaults write -g com.apple.mouse.scaling 7
