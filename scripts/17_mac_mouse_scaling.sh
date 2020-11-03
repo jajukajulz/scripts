017793614294ff00fe3f1d4c0b0b5a71815cba81
Julian Kanjere <2832754+jajukajulz@users.noreply.github.com>
2020-11-03 12:59:27 +0200

    Update and rename mac_mouse_scaling.sh to 17_mac_mouse_scaling.sh

--- scripts/17_mac_mouse_scaling.sh
+++ scripts/17_mac_mouse_scaling.sh
@@ -2,7 +2,7 @@
 
 #How to speed up mouse tracking on a Mac
 #$ chmod a+x 17_mac_mouse_scaling.sh
-#$ ./mac_mouse_scaling.sh
+#$ ./17_mac_mouse_scaling.sh
 
 #read current scaling speed (deafult is 3)
 defaults read -g com.apple.mouse.scaling
